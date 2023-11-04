fe822728401e82398846b490a468375fe44c48f9
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-11-04 09:16:16 +0100

    first a commit

--- src/app/components/sentences/HeroRight.tsx
+++ src/app/components/sentences/HeroRight.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import HeroSVG from '../words/HeroSVG';
+
+
 
 function HeroRight() {
   return (
-    <div className='w-66.625rem h-41.375rem'>
-      <span>OUR MISSION</span>
+    <div className="flex flex-col w-[66.625rem] gap-[1.06rem]">
+      <span className="flex w-[8.5rem] items-start justify-center relative before:absolute before:content-[''] before:w-3 before:h-[2px] before:bg-green-600 before:left-0 before:top-4">
+        OUR MISSION
+      </span>
+      <h1 className="w-[70%] text-[2.5rem] leading-[2.8125rem]">
+        We decided to carry the flag for rare diseases in Saudi Arabia
+      </h1>
+      <HeroSVG/>
     </div>
-  )
+  );
 }
 
 export default HeroRight
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroRight from './components/sentences/HeroRight'
 
 function page() {
   return (
-    <div className='font-[Exo_2] flex flex-col gap-4 items-center justify-center w-90rem mx-auto bg-white'>
+    <div className='font-[Exo_2] flex flex-col gap-4 items-center justify-center w-[90rem] mx-auto bg-white'>
       <Navbar/>
       <HeroRight/>
     </div>
